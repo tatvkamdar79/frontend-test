02cd92371572ab65022ec3ee768f1169665f01a2
Tatva Kamdar <kamdartatv1@gmail.com>
2023-09-07 16:16:57 +0530

    Config restored

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://backend-test-black.vercel.app";
+// export const baseUrl = "http://localhost:8080";
 export const productAPI = "/product";
-// export const baseUrl = "https://backend-test-black.vercel.app";
