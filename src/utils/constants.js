7a53f6bf0b42d6855ff6a1d0c07d40be800f0eda
tatvkamdar79 <kamdartatv79@gmail.com>
2023-09-28 15:13:00 +0530

    URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const baseUrl = "https://backend-test-black.vercel.app";
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://backend-test-black.vercel.app";
+// export const baseUrl = "http://localhost:8080";
 export const productAPI = "/product";
 export const adminAPI = "/admin";
